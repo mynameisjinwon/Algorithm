9fcb3f25eb3ea202a35b4294cabc33b466907e22
Hanjinwon <32839363+mynameisjinwon@users.noreply.github.com>
2022-05-08 23:36:10 +0900

    Create 2743.java

---
+++ baekjoon/단계별로풀기/2743.java
@@ -0,0 +1,14 @@
+/*
+https://www.acmicpc.net/problem/2743
+단어 길이 재기
+*/
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        
+        System.out.println(bf.readLine().length());
+    }
+}
