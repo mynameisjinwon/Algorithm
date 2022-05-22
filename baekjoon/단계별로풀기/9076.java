e13f296b07a815e5fc6349070cf7373eec7ae84d
Hanjinwon <32839363+mynameisjinwon@users.noreply.github.com>
2022-05-23 00:14:14 +0900

    Update 9076.java

--- baekjoon/단계별로풀기/9076.java
+++ baekjoon/단계별로풀기/9076.java
@@ -2,28 +2,41 @@
 https://www.acmicpc.net/problem/9076
 점수 집계
 */
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    @SuppressWarnings("resource")
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int N = Integer.parseInt(sc.nextLine());
-        for (int i = 0; i < N; i++) {
-            String[] str = sc.nextLine().split(" ");
-            int[] arr = new int[str.length];
-            for (int j = 0; j < arr.length; j++)
-                arr[j] = Integer.parseInt(str[j]);
-            Arrays.sort(arr);
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
-            String ret = "";
-            if (arr[arr.length - 2] - arr[1] >= 4)
-                ret = "KIN";
-            else
-                ret = arr[1] + arr[2] + arr[3] + "";
-            System.out.println(ret);
+        final int MAX_DIFF = 4;
+        final int SCORE_CNT = 5;
+
+        int N = Integer.parseInt(bf.readLine());
+
+        for(int i=0;i<N;i++) {
+            ArrayList<Integer> arr = new ArrayList<>();
+            st = new StringTokenizer(bf.readLine());
+            for(int j=0;j<SCORE_CNT;j++) {
+                arr.add(Integer.parseInt(st.nextToken()));
+            }
+            Collections.sort(arr);
+            arr.remove(0);
+            arr.remove(arr.size()-1);
+            if(arr.get(arr.size()-1) - arr.get(0) >= MAX_DIFF) {
+                sb.append("KIN").append("\n");
+                continue;
+            }
+            int sum = 0;
+            for(int n : arr) {
+                sum += n;
+            }
+            sb.append(sum).append("\n");
         }
+        System.out.println(sb);
     }
 }
