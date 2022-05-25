8e9a7571401a373a90c6d0115a0cedc0de52282a
Hanjinwon <32839363+mynameisjinwon@users.noreply.github.com>
2022-05-25 22:24:36 +0900

    Create 5337.java

---
+++ baekjoon/단계별로풀기/5337.java
@@ -0,0 +1,15 @@
+/*
+https://www.acmicpc.net/problem/5337
+웰컴
+*/
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(".  .   .").append("\n");
+        sb.append("|  | _ | _. _ ._ _  _").append("\n");
+        sb.append("|/\\|(/.|(_.(_)[ | )(/.");
+
+        System.out.println(sb);
+    }
+}
