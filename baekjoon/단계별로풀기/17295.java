20707e1611d1d506d7cb76e96e9611faac582c69
Hanjinwon <32839363+mynameisjinwon@users.noreply.github.com>
2022-05-26 22:09:57 +0900

    Create 17295.java

---
+++ baekjoon/단계별로풀기/17295.java
@@ -0,0 +1,14 @@
+/*
+https://www.acmicpc.net/problem/17295
+엔드게임 스포일러
+*/
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        bf.readLine();
+        System.out.println("Avengers: Endgame");
+    }
+}
