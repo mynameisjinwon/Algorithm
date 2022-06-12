87ea09056e37fe58fb8430344bfa318b90ceb925
Hanjinwon <32839363+mynameisjinwon@users.noreply.github.com>
2022-06-12 22:27:26 +0900

    Create 2443.java

---
+++ baekjoon/단계별로풀기/2443.java
@@ -0,0 +1,40 @@
+/*
+https://www.acmicpc.net/problem/2443
+별 찍기 - 6
+*/
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    private static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bf.readLine());
+
+        int spaceCnt = 0, asteriskCnt = N * 2 - 1;
+
+        for(int i=0;i<N*2-1;i++) {
+            printSpace(spaceCnt);
+            printAsterisk(asteriskCnt);
+
+            spaceCnt++;
+            asteriskCnt -= 2;
+        }
+
+        System.out.println(sb);
+    }
+    private static void printSpace(int spaceCnt) {
+        for(int i=0;i<spaceCnt;i++) {
+            sb.append(" ");
+        }
+    }
+    private static void printAsterisk(int asteriskCnt) {
+        for(int i=0;i<asteriskCnt;i++) {
+            sb.append("*");
+        }
+        sb.append("\n");
+    }
+}
