1af6e6963fe2cb83b823259f5bdeb91f786ac63d
Hanjinwon <32839363+mynameisjinwon@users.noreply.github.com>
2022-06-23 21:19:56 +0900

    Create 16394.java

---
+++ baekjoon/단계별로풀기/16394.java
@@ -0,0 +1,72 @@
+/*
+https://www.acmicpc.net/problem/16394
+홍익대학교
+*/
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static MyReader scan = new MyReader();
+    private static StringBuilder sb = new StringBuilder();
+    private static int N;
+    private static final int BASE = 1946;
+
+    public static void main(String[] args) {
+        input();
+        solution();
+        printResult();
+    }
+    private static void printResult() {
+        System.out.println(sb);
+    }
+    private static void solution() {
+        sb.append(N - BASE);
+    }
+    private static void input() {
+        N = scan.nextInt();
+    }
+    private static class MyReader {
+        BufferedReader bf;
+        StringTokenizer st;
+
+        private MyReader() {
+            bf = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        private String next() {
+            while( st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(bf.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        private int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        private long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        private double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        private String nextLine() {
+            String str = "";
+            try {
+                str = bf.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
