73f74f5736d03c6acd4f0db75d34cd22c06b5859
Hanjinwon <32839363+mynameisjinwon@users.noreply.github.com>
2022-06-30 19:43:33 +0900

    Create 5596.java

---
+++ baekjoon/단계별로풀기/5596.java
@@ -0,0 +1,76 @@
+/*
+https://www.acmicpc.net/problem/5596
+시험점수
+*/
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static MyReader scan = new MyReader();
+    private static StringBuilder sb = new StringBuilder();
+    private static int[] score;
+    private static final int STUDENT = 2, SUBJECT = 4;
+
+    public static void main(String[] args) {
+        solution();
+        printResult();
+    }
+    private static void printResult() {
+        System.out.println(sb);
+    }
+    private static void solution() {
+        score = new int[2];
+
+        for(int i=0;i<STUDENT;i++) {
+            for(int j=0;j<SUBJECT;j++) {
+                score[i] += scan.nextInt();
+            }
+        }
+        sb.append(Math.max(score[0], score[1]));
+    }
+
+    private static class MyReader {
+        BufferedReader bf;
+        StringTokenizer st;
+
+        private MyReader() {
+            bf = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        private String next() {
+            while( st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(bf.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        private int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        private long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        private double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        private String nextLine() {
+            String str = "";
+            try {
+                str = bf.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
