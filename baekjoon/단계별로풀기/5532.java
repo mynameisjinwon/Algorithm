fa38a1860067abefe13be4a743841aa356f9326c
Hanjinwon <32839363+mynameisjinwon@users.noreply.github.com>
2022-07-19 20:32:33 +0900

    Create 5532.java

---
+++ baekjoon/단계별로풀기/5532.java
@@ -0,0 +1,82 @@
+/*
+https://www.acmicpc.net/problem/5532
+방학 숙제
+*/
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static MyReader scan = new MyReader();
+    private static StringBuilder sb = new StringBuilder();
+    private static int L, A, B, C, D;
+
+    public static void main(String[] args) {
+        input();
+        solution();
+        printResult();
+    }
+    private static void printResult() {
+        System.out.println(sb);
+    }
+    private static void solution() {
+        int ans = 0;
+
+        int korean = (int)Math.ceil((double)A / C);
+        int math = (int)Math.ceil((double)B / D);
+
+        ans = L - Math.max(korean, math);
+
+        sb.append(ans);
+    }
+    private static void input() {
+        L = scan.nextInt();
+        A = scan.nextInt();
+        B = scan.nextInt();
+        C = scan.nextInt();
+        D = scan.nextInt();
+    }
+    private static class MyReader {
+        BufferedReader bf;
+        StringTokenizer st;
+
+        private MyReader() {
+            bf = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        private String next() {
+            while( st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(bf.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        private int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        private long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        private double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        private String nextLine() {
+            String str = "";
+            try {
+                str = bf.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
